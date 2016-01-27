9a4f9c6f74c270d66f3943f6c86d081844cc7675
Valtteri Mäntylä-Blå <valtteri.mantyla@gmail.com>
2016-01-27 11:15:33 +0200

    mittausraporttiharjoitus

--- IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Harjoitus1MediaPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,6 @@ namespace Harjoitus1MediaPlayer
         public MainWindow()
         {
             InitializeComponent();
-            LoadMediaFile();
         }
 
         private void LoadMediaFile()
@@ -31,12 +31,16 @@ namespace Harjoitus1MediaPlayer
             try
             {
                 // soitetaan käyttäjän valitsemaa mediatiedostoa
-                string filu = @"D:\H8374\test.mp4";
+                // string filu = @"D:\H8374\test.mp4";
+                string filu = txtFileName.Text;
                 // tutkitaan onko tiedosto olemassa
                 if (System.IO.File.Exists(filu))
                 {
                     // MessageBox.Show("soitetaan tiedosto: " + filu);
                     mediaElement.Source = new Uri(filu);
+                }
+                else {
+                    MessageBox.Show("Tiedostoa " + filu + " ei löydy.");
                 }
 
             }
@@ -48,17 +52,44 @@ namespace Harjoitus1MediaPlayer
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
+            LoadMediaFile();
             mediaElement.Play();
+            ChangeButtonState();
         }
 
         private void btnPause_Click(object sender, RoutedEventArgs e)
         {
             mediaElement.Pause();
+            ChangeButtonState();
         }
 
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             mediaElement.Stop();
+            ChangeButtonState();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            // avataan vakio open-dialogi jotta käyttäjä voi valita tiedoston
+            OpenFileDialog dlg = new OpenFileDialog(); // käytä tarjottua importtia
+            dlg.InitialDirectory = "D:\\"; // oletuskansio josta tiedoston haku lähtee
+            dlg.Filter = "Rock files(*.mp3)|*.mp3|Media files (*.wmv, *.mp4)|*.wmv,*.mp4,*.mpeg4|All files (*.*)|*.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                txtFileName.Text = dlg.FileName;
+                btnPlay.IsEnabled = true;
+                btnPause.IsEnabled = false;
+                btnStop.IsEnabled = false;
+            }
+        }
+
+        private void ChangeButtonState()
+        {
+            btnPause.IsEnabled = !btnPause.IsEnabled;
+            btnStop.IsEnabled = !btnStop.IsEnabled;
+            btnPlay.IsEnabled = !btnPlay.IsEnabled;
         }
     }
 }
