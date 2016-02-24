2147f43c048ed0c2c4a1fba11f7b0981a0633dea
Valtteri Mäntylä-Blå <valtteri.mantyla@gmail.com>
2016-02-24 11:21:27 +0200

    morehockey

---
+++ IIO11300Vktehtavat/bindingDemo/Hockey.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bindingDemo
+{
+    public class HockeyTeam
+    {
+        #region PROPERTIES
+        // huom public field ei kelpaa WPF:n databindingissä, pitää olla Property
+        public string Name { get; set; }
+        public string City { get; set; }
+        #endregion
+        #region CONSTRUCTORS
+        public HockeyTeam()
+        {
+            Name = "Anon";
+            City = "Unknown";
+        }
+        public HockeyTeam (string name, string city)
+        {
+            Name = name;
+            City = city;
+        }
+        #endregion
+        public override string ToString()
+        {
+            return Name + "@" + City;
+        }
+    }
+
+    
+
+    public class HockeyLeague
+    {
+        // perustetaan SM-liiga joka sisältää x kpl joukkueita
+        // HUOM! jos halutaan, että databindaus huomaa muutokset kokoelmassa automaattisesti, kaytä ObservableCollection-kokoelmaa 
+        List<HockeyTeam> teams = new List<HockeyTeam>();
+        public HockeyLeague()
+        {
+            teams.Add(new HockeyTeam("HIFK", "Helsinki"));
+            teams.Add(new HockeyTeam("JYP", "Jyväskylä"));
+            teams.Add(new HockeyTeam("Kalpa", "Kuopio"));
+            teams.Add(new HockeyTeam("Sport", "Vaasa"));
+        }
+        // metodi joka palauttaa liigan joukkueet
+        public List<HockeyTeam> GetTeams()
+        {
+            return teams;
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/bindingDemo/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bindingDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // moduulitason muuttujat
+        HockeyLeague smliiga;
+        List<HockeyTeam> joukkueet;
+        int clicked = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            fillMyCombox();
+            smliiga = new HockeyLeague();
+            joukkueet = smliiga.GetTeams();
+        }
+
+        private void fillMyCombox()
+        {
+            cbCourses2.Items.Add("IIO12110 Ohjelmistotuotanto");
+            cbCourses2.Items.Add("DED666 Työelämän ruåtsi");
+            cbCourses2.Items.Add("J3EEE Kesäloma");
+        }
+
+        private void btnSetUser_Click(object sender, RoutedEventArgs e)
+        {
+            txtUserName.Text = "Helou " + bindingDemo.Properties.Settings.Default.UserName;
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            myGrid.DataContext = joukkueet;
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            clicked++;
+            if (clicked > 3) clicked = 3;
+            myGrid.DataContext = joukkueet[clicked];
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+            clicked--;
+            if (clicked < 0) clicked = 0;
+            myGrid.DataContext = joukkueet[clicked];
+        }
+    }
+}
